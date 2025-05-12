a1b15f2943c329c8ca2cf4778437a09bc0a38398
Maria Norman <signemaria83@gmail.com>
2025-05-12 14:44:38 +0200

    Försöker få det att fungera med GoLive

---
+++ Javascript/Index.js
@@ -0,0 +1,8 @@
+//Detta är en kommentar och kommer inte tolkas av läsaren som kod. 
+let age = 41; //Number
+let myName = "Maria"; // String
+const isStudent =true; // boolean (true or false)
+
+
+console.log(myName);
+console.log("Hej")

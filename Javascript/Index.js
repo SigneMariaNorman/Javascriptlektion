eda74cfe3e675ae3e9d375816634ef1bb228a6c8
Maria Norman <signemaria83@gmail.com>
2025-05-12 17:13:25 +0200

    Update Index.js

--- Javascript/Index.js
+++ Javascript/Index.js
@@ -7,13 +7,19 @@ const isStudent =true; // boolean (true or false)
 console.log("Hej" + myName + ", jag är " + age);
 console.log("Är student: " + isStudent);
 console.log(myName, "Hej");
-let greeting = "hej, jag heter " + myName + ", jag är" + age + "år";
-if (myName == "Ada") {
-    console.log('Du heter Ada!');
+let greeting = "hej, jag heter " + myName + ", jag är " + age + "år";
+
+    if(myName == "Ada") {
+        console.log("Du heter Ada");
+    } else {
+        console.log("Du heter inte Ada")
+    }
+if (age > 36) { // >större än, <
+    console.log('Ung i hjärtat');
 }
 console.log(greeting);
-else {
-    if(myName == "Ada"){
-        console.log("Du heter inte Ada");
-    }
+else if () {
+     // ==jämförelse, // = eller, 
+}
+    else {
 }

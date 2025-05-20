ee9ed41c92d46cea8c1b710609eea6083e995d46
Maria Norman <signemaria83@gmail.com>
2025-05-20 10:38:45 +0200

    Update Index.js

--- Javascript/Index.js
+++ Javascript/Index.js
@@ -1,28 +1,12 @@
-function add(numberOne, numberTwo) {
-    console.log('Add');
-    console.log('NumberOne är', numberOne);
-    console.log('numberTwo är', numberTwo);
-    let sum = numberOne + numberTwo;
-   
-    console.log("summan blir: " + sum);
-}
+/**
+ * Prompt for user choice of weapon
+ * SET variable to userChoice
+ * Random computers weapon
+ * Set variable to computerChoice
+ * Determine winner by if-statements
+ * Show winner
+ */
 
-function subtract(numberOne, numberTwo) {
-    console.log('Subtract')
-    let sum = numberOne - numberTwo;
 
-    console.log("Summan blir: " + sum);
-}
-
-const inputMunber = prompt('Välj första nummer');
-console.log('Första nummer: ', inputNumberOne);
-const inputOperator = prompt("Välj addering (+) eller subtrahering (-)");
-console.log("Operator:", inputOperator);
-const inputNumberTwo = prompt("Välj andra nummer");
-console.log('A nummer: ', inputNumberTwo);
-
-if (inputOperator == '+') {
-    add(inputNumberOne, inputNumberTwo);
-} else if (inputOperator == '-'){
-    subtract(inputNumberOne, inputNumberTwo);
-}
+const userchoice = prompt('Välj vapen: sten, sax eller påse'); //Möjliga värden: "sten", "sax" eller "påse"
+console.log(userchoice);
